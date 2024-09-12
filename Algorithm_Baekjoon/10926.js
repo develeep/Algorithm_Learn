5b0970171ec4c372f7d8b8443f01f146009c07b1
Lee <dltmdgns0508@gmail.com>
2024-09-12 22:20:35 +0900

    알고리즘 풀이 기초 설정 및 예시 문제 풀이3개

---
+++ Algorithm_Baekjoon/10926.js
@@ -0,0 +1,6 @@
+const input = require('fs')
+  .readFileSync('/dev/stdin')
+  .toString()
+  .trim()
+  .split(' ');
+console.log(input[0] + '??!');
---
+++ Algorithm_Baekjoon/example.js
@@ -0,0 +1,8 @@
+const input = require('fs')
+  .readFileSync('/dev/stdin')
+  .toString()
+  .trim()
+  .split('\n');
+const a = +input[0];
+const b = +input[1];
+console.log(a + b);
