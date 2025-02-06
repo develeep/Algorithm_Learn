cbfe8fa4236986f2cd9d0a50d628c191f582906d
develeep <dltmdgns0508@gmail.com>
2025-02-06 16:02:30 +0900

    swea GNS 문제풀이

---
+++ swea/0206/gns/main.py
@@ -0,0 +1,37 @@
+import sys
+
+sys.stdin = open('input.txt', 'r')
+##################################################
+NUM = {
+    "ZRO": 0,
+    "ONE": 1,
+    "TWO": 2,
+    "THR": 3,
+    "FOR": 4,
+    "FIV": 5,
+    "SIX": 6,
+    "SVN": 7,
+    "EGT": 8,
+    "NIN": 9,
+}
+
+key = list(NUM.keys())
+
+T = int(input())  # Testcase 개수를 받아오는 코드
+for tc in range(1, T + 1):
+    t, length = input().split()
+    str_arr = input().split()
+    arr = []
+    result = ''
+    count = [0] * 10
+
+    for s in str_arr:
+        n = NUM[s]
+        count[n] += 1
+
+    for idx in range(len(count)):
+        if count[idx]:
+            result += (key[idx] + ' ') * count[idx]
+
+    print(t)
+    print(result)
