a146ab28078d68dc8420390594e6bb6e3f8ea314
develeep <dltmdgns0508@gmail.com>
2025-02-27 23:32:13 +0900

    swea shortpath 문제풀이

--- swea/0226/shortPath/main.py
+++ swea/0226/shortPath/main.py
@@ -3,37 +3,38 @@ sys.stdin = open('input.txt', 'r')
 ##################################################
 import heapq
 
-class Node:
-    def __init__(self,index):
-        self.index = index
-        self.cnt = float('inf')
-        self.neighbor = []
-
-    def push_neighbor(self,node,cnt):
-        self.neighbor.append((node,cnt))
-
+# class Node:
+#     def __init__(self,index):
+#         self.index = index
+#         self.cnt = float('inf')
+#         self.neighbor = []
+#
+#     def push_neighbor(self,node,cnt):
+#         self.neighbor.append((node,cnt))
 
 T = int(input())    # Testcase 개수를 받아오는 코드
 for tc in range(1, T + 1):
     n,m,start,end = map(int,input().split())
-    arr = [Node(i) for i in range(n)]
+    # arr = [Node(i) for i in range(n)]
+    graph = [[] for _ in range(n)]
     for _ in range(m):
-        l,r,cnt = map(int,input().split())
-        arr[l-1].push_neighbor(arr[r-1],cnt)
-        arr[r-1].push_neighbor(arr[l-1],cnt)
+        l, r, cnt = map(int,input().split())
+        graph[l-1].append((r-1, cnt))
+        graph[r-1].append((l-1, cnt))
 
-    arr[start-1].cnt = 0
+    min_cnt = [float('inf')]*n
+    min_cnt[start-1] = 0
     move_que = []
-    heapq.heappush(move_que, (0,arr[start-1]))
+    heapq.heappush(move_que, (0, start-1))
 
     while move_que:
-        move_cnt, move_node = heapq.heappop(move_que)
-        if move_node == arr[end-1]:
+        move_cnt, move_idx = heapq.heappop(move_que)
+        if move_idx == end-1:
             break
-        for node,move in move_node.neighbor:
-            move_c = move + move_cnt
-            if move_c < node.cnt:
-                node.cnt = move_c
-                heapq.heappush(move_que, (move_c, node))
+        for index, cnt in graph[move_idx]:
+            move = cnt + move_cnt
+            if move < min_cnt[index]:
+                min_cnt[index] = move
+                heapq.heappush(move_que,(move, index))
 
-    print(f'#{tc}',arr[end-1].cnt)
+    print(f'#{tc}', min_cnt[end-1])
