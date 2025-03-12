0ced978b991ac37a1653d3a7e6c8a59c2c028520
develeep <dltmdgns0508@gmail.com>
2025-03-12 09:11:42 +0900

    swea 등산로 조ì 풀이 ì 수정

--- swea/0228/mountain/main.py
+++ swea/0228/mountain/main.py
@@ -1,35 +1,57 @@
 import sys
 sys.stdin = open('input.txt', 'r')
 ##################################################
-import heapq
-dxy = [(0,1),(0,-1),(1,0),(-1,0)]
-def dfs(x,y,special,depth):
-    if special==0:
+dxy = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+
+
+def dfs(x, y, special, depth):
+    global max_result
+
+    flag = 0
+    for dx, dy in dxy:
+        nx, ny = x + dx, y + dy
+        if not (0 <= nx < n and 0 <= ny < n):
+            continue
+
+        if visited[nx][ny]:
+            continue
+
+        if arr[nx][ny] < arr[x][y]:
+            flag = 1
+            visited[nx][ny] = 1
+            dfs(nx, ny, special, depth + 1)
+            visited[nx][ny] = 0
+            continue
+
+        if special and arr[nx][ny] - k < arr[x][y]:
+            flag = 1
+            visited[nx][ny] = 1
+            old = arr[nx][ny]
+            arr[nx][ny] = arr[x][y] - 1
+            dfs(nx, ny, 0, depth + 1)
+            arr[nx][ny] = old
+            visited[nx][ny] = 0
+
+    if flag == 0:
+        max_result = max(max_result, depth)
         return
 
-    possible_points = []
-    for dx,dy in dxy:
-        nx,ny = x+dx, y+dy
-        if not(0<=nx<n and 0<=ny<n):
-            continue
-        if arr[nx][ny] < arr[x][y]:
-            possible_points.append((arr[nx][ny],nx,ny,0))
-            continue
 
-
-T = int(input())    # Testcase 개수를 받아오는 코드
+T = int(input())  # Testcase 개수를 받아오는 코드
 for tc in range(1, T + 1):
-    n,k = map(int,input().split())
-    arr = [tuple(map(int,input().split())) for _ in range(n)]
-    visited = [[0]*n for _ in range(n)]
+    n, k = map(int, input().split())
+    arr = [list(map(int, input().split())) for _ in range(n)]
     cache = {}
     max_h = 0
     for i in range(n):
         for j in range(n):
             max_h = max(max_h, arr[i][j])
 
-    high_mountain = [(i,j) for i in range(n) for j in range(n) if arr[i][j] == max_h]
+    high_mountain = [(i, j) for i in range(n) for j in range(n) if arr[i][j] == max_h]
+    max_result = 1
+    for hx, hy in high_mountain:
+        visited = [[0] * n for _ in range(n)]
+        visited[hx][hy] = 1
+        dfs(hx, hy, 1, 1)
 
-    for hx,hy in high_mountain:
-        visited[(hx,hy)] = 1
-        dfs(hx,hy)
+    print(f'#{tc}', max_result)
