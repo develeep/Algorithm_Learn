b7234324f6dd376fd8c6fb1c8362bc17c3d78827
develeep <dltmdgns0508@gmail.com>
2025-04-03 01:53:20 +0900

    udpate pinbole

--- swea/0402/pinbole/main.py
+++ swea/0402/pinbole/main.py
@@ -7,28 +7,28 @@ walls = ((1,3,0,2),(3,0,1,2),(2,0,3,1),(1,2,3,0))
 block = {0:1,1:0,2:3,3:2}
 
 def dfs(x,y,d,cnt):
-    history = []
+    history = [(x,y)]
     nx,ny = x,y
     while 1:
         nx, ny = nx + dxy[d][0], ny + dxy[d][1]
         if not(0<=nx<n and 0<=ny<n):
-            cnt += (cnt*2) + 1
+            cnt = (cnt*2) + 1
             break
-        if grid[x][y] == -1:
+        if grid[nx][ny] == -1:
             break
         if his[nx][ny][d]:
             cnt += his[nx][ny][d]
             break
-        if grid[x][y] == 5:
-            cnt += (cnt*2) + 1
+        if grid[nx][ny] == 5:
+            cnt = (cnt*2) + 1
             break
-        if 1<=grid[x][y] < 5:
+        if 1 <= grid[nx][ny] < 5:
             change_d = walls[grid[nx][ny] - 1][d]
             if block[change_d] == d:
-                cnt += (cnt * 2) + 1
+                cnt = (cnt * 2) + 1
                 break
             else:
-                cnt += dfs(nx, ny, change_d, cnt+1)
+                cnt = dfs(nx, ny, change_d, 0) + 2
                 break
         else:
             history.append((nx,ny))
@@ -44,7 +44,7 @@ def dfs(x,y,d,cnt):
 
 
 T = int(input())    #test case 개수를 받아오는 코드
-for tc in range(1, 1+1):
+for tc in range(1, T+1):
     n = int(input())
     grid = tuple(tuple(map(int,input().split())) for _ in range(n))
     his = [[[0,0,0,0] for _ in range(n)] for _ in range(n)]
@@ -63,5 +63,4 @@ for tc in range(1, 1+1):
                         continue
                     res = dfs(i,j,d,0)
                     result = max(res,result)
-    print(*his, sep='\n')
     print(f"#{tc}", result)
